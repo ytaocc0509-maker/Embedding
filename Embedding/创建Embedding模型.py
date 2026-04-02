dc55ff824f48b7ffd4cd05286365374bc8b10825
ytaocc0509-maker <ytaocc0509@gmail.com>
2026-04-02 23:50:53 +0800

    Update 创建Embedding模型.py

--- Embedding/创建Embedding模型.py
+++ Embedding/创建Embedding模型.py
@@ -39,7 +39,7 @@ df = df[df.count_token <= max_token].tail(top_n)
 # 初始化openai的客户端
 client = OpenAI(  # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：api_key="sk-xxx",
     # 各地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
-    api_key="sk-e7a344ef54e842dc8bfc289a7dce66bc",  # os.getenv("DASHSCOPE_API_KEY"),
+    api_key="",  # os.getenv("DASHSCOPE_API_KEY"),
     # 以下是北京地域base-url，如果使用新加坡地域的模型，需要将base_url替换为：https://dashscope-intl.aliyuncs.com/compatible-mode/v1
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1")
 
